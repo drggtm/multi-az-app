59ab0d3808454544a484bae8af0c8f4c40762868
Durga <drggtm07@gmail.com>
2025-06-12 15:09:21 +0545

    main file added

---
+++ main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_availability_zone" "available" {
+  state = "available"
+}
+
+module "networking" {
+  source = "./modules/networking"
+  vpc_cidr = var.vpc_cidr
+  environment = var.environment
+  project_name = var.project_name
+  availability_zones_count = var.availability_zones_count
+  tags= var.tags
+  common_tags = var.common_tags
+}
+
+module "security" {
+  source = "./modules/security"
+  project_name = var.project_name
+  environment = var.environment
+  vpc_id = module.networking.vpc_id
+  common_tags = var.common_tags
+  enable_ssh_access = var.enable_ssh_access
+  ssh_cidr_blocks = var.ssh_cidr_blocks
+}
+
+module "compute" {
+  source = "./modules/compute"
+  project_name = var.project_name
+  environment = var.environment
+  instance_type = var.instance_type
+  key_pair_name = var.key_pair_name
+  ec2_security_group_id = module.ec2_security_group_id
+  app_version = var.app_version
+  common_tags = var.common_tags
+  vpc_id = module.networking.vpc_id
+  pubic_subnet_ids = module.networking.public_subnets_ids
+  min_size = var.min_size
+  max_size = var.max_size
+  desired_capacity = var.desired_capacity
+  private_subnet_ids = var.private_subnet_ids
+}
--- variables.tf
+++ variables.tf
@@ -19,3 +19,84 @@ variable "availability_zones_count" {
     error_message = "Availability zones count must be between 2 and 4."
   }
 }
+
+variable "aws_region" {
+  description = "Aws region"
+  type = string
+  default = "us-east-2"
+}
+
+variable "environment" {
+description = "environment using service"
+type= string
+default = "dev"  
+}
+
+variable "common_tags" {
+  description = "tags to apply for all resourec"
+  type = map(string)
+  default = {
+  }
+}
+
+variable "tags" {
+  description = "tags for vpc"
+  type = map(string)
+  default = {
+  
+  }
+}
+
+variable "instance_type" {
+  description = "type of ec2 instance"
+  type = string
+  default = "t2.micro"
+}
+
+variable "key_pair_name" {
+  description = "key pair for ec2 instance creation"
+  type = string
+  default = {}
+}
+
+variable "app_version" {
+  description = "version off app"
+  type = string
+  default = "1.0.0"
+}
+
+variable "min_size" {
+  description = "minimu number of ec2 for autoscaling"
+  type = number
+  default = 1
+}
+
+variable "max_size" {
+  description = "maximum number of ec2 for asg"
+  type = number
+  default = 3
+}
+
+variable "desired_capacity" {
+  description = "desired number of instance"
+  type = number
+  default = 2
+}
+
+variable "private_subnet_ids" {
+  description = "Ids of private subnets for ec2"
+  type = list(string)
+  default = [  ]
+}
+
+variable "enable_ssh_access" {
+  description = "ebable ssh access to ec2?"
+  type = bool
+  default = false
+}
+
+variable "ssh_cidr_blocks" {
+  description = "cidr block for ssh"
+  type = list(string)
+  default = [  ]
+}
